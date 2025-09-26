73e3e260b5dfda7665c123838d74c6fe071b7239
peter <petersp2000@gmail.com>
2025-09-26 15:29:42 +0100

    changed if structure

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -12,7 +12,7 @@ def perform_operation(num1, num2, operation):
         case "divide":
             if num2 == 0:
                 result = "Cannot divide by zero"
-            else:
+            elif num2 != 0:
                 result = num1 / num2
 
     return result
