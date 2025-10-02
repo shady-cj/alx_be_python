f25194cb8c609ac3c3f58b15fd019d91cc4407d1
shady-cj <petersp2000@gmail.com>
2025-10-02 20:58:27 +0100

    renamed test methods

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -21,14 +21,14 @@ class TestSimpleCalculator(unittest.TestCase):
         self.assertEqual(self.calc.subtract(0, 5), -5)
         self.assertEqual(self.calc.subtract(4, 6), -2)
 
-    def test_multiply(self):
+    def test_multiplication(self):
         """Test the multiply method"""
         self.assertEqual(self.calc.multiply(1, 0), 0)
         self.assertEqual(self.calc.multiply(5, 2), 10)
         self.assertEqual(self.calc.multiply(-5, 5), -25)
         self.assertEqual(self.calc.multiply(-5, -5), 25)
 
-    def test_divide(self):
+    def test_division(self):
         """Test the divide method"""
         self.assertEqual(self.calc.divide(4, 2), 2)
         self.assertEqual(self.calc.divide(4, -2), -2)
